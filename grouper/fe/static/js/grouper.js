bbe192465a0f01d7deb1021afc28f9f1f3101838
Rick Mark <rickmark@dropbox.com>
2018-05-08 20:44:17 -0700

    Remove inline script where simple

--- grouper/fe/static/js/grouper.js
+++ grouper/fe/static/js/grouper.js
@@ -19,4 +19,73 @@ $(document).ready(function(){
     } catch(e) {
         console.log("error in datatable init: ", e);
     }
+
+    $("#createModal").on("shown.bs.modal", function(){
+        $("#tagname").focus();
+    });
+
+    $("#formSubmit").click(function() {
+        $("#createFrom").submit();
+    });
+
+
+    $("#revokeModal").on("show.bs.modal", function(e) {
+        var button = $(e.relatedTarget);
+        var mappingId = button.data("mapping-id");
+
+        var modal = $(e.currentTarget);
+
+        var form = modal.find(".revoke-permission-form")
+        form.attr("action", "/groups/{{group.name}}/service/{{user.username}}/revoke/" + mappingId);
+    });
+
+    $("#removeUserModal").on("show.bs.modal", function(e) {
+        var button = $(e.relatedTarget);
+        var memberName = button.data("member-name");
+        var memberType = button.data("member-type");
+
+        var modal = $(e.currentTarget);
+        modal.find(".member-name").html(memberName);
+
+        var form = modal.find(".remove-member-form");
+        form.attr("action", "/groups/{{ group.id }}/remove");
+        form.find("input[name=member]").val(memberName);
+        form.find("input[name=member_type]").val(memberType);
+    });
+
+    $('#join-form-expiration').datetimepicker({
+        pickTime: false,
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-arrow-up",
+            down: "fa fa-arrow-down"
+        },
+        useCurrent: false,
+        minDate: moment(),
+    });
+
+    $('#edit-form-expiration').datetimepicker({
+        pickTime: false,
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-arrow-up",
+            down: "fa fa-arrow-down"
+        },
+        useCurrent: false,
+        minDate: moment(),
+    });
+
+    $('#audit-form-ends-at').datetimepicker({
+        pickTime: false,
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-arrow-up",
+            down: "fa fa-arrow-down"
+        },
+        useCurrent: false,
+        minDate: moment(),
+    });
 });
